5be78c4d55aac90a5e93fd3e5fbe7b5a2ce8e222
JmAlmighty <justinc@cwghp.ca>
2025-10-22 17:46:31 -0400

    how

--- extract_snippet.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-text = Path("sample_page.html").read_text(encoding="utf-8")
-marker = '<div class="pd__space pdp_new_design">'
-idx = text.find(marker)
-print("idx:", idx)
-if idx != -1:
-    print(text[idx:idx + 12000])
